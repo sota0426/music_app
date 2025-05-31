e4a8085c2442f5abd649787c8af7f14dec2c3042
sota0426 <sota0426@gmail.com>
2025-05-31 11:40:51 +0900

    remote

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import { Figtree } from "next/font/google";
+
+import Sidebar from "@/components/Sidebar";
+
+const font = Figtree({subsets:["latin"]})
+
+export const metadata={
+  title:"Spotify Clone",
+  description:"Listen to music";
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.classname}
+      >
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  );
+}
