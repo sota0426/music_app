7cb935fd9b857d4cb60e1409a594f7585c82d597
sota0426 <46316662+sota0426@users.noreply.github.com>
2025-06-08 17:59:08 +0900

    search yet

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images:{
+    domains:[
+      "ruikkiquinzlnhzfutuw.supabase.co"
+    ]
+  },
+};
+
+export default nextConfig;
